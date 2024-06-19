adfce760778cf2a9cebc46bf7cbce77449be0df8
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-19 12:52:14 -0500

    Update and rename GeneralComputer to GeneralComputer.lua

--- GeneralComputer.lua
+++ GeneralComputer.lua
@@ -20,26 +20,26 @@ functions = {
 
 	ping = function(commands)
 
-        local name = commands[2]
+		local name = commands[2]
 
-        --No player given
-        if name == nil or name == "" then
+		--No player given
+		if name == nil or name == "" then
 			chatFunctions.privateMessage({text = "Enter an player username.", color = "red", bold = true}, commands[1])
 			return
 		end
 
-        local table = det.getPlayerPos(name)
+		local table = det.getPlayerPos(name)
 		local user = det.getPlayerPos(commands[1])
 
-        if table.x == nil then
-            chatFunctions.privateMessage({text = "Invalid username or player is in another dimension.", color = "red", bold = true}, commands[1])
+		if table.x == nil then
+			chatFunctions.privateMessage({text = "Invalid username or player is in another dimension.", color = "red", bold = true}, commands[1])
 			return
-        end
-        
-        chatFunctions.privateMessage({text = commands[2] .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z, color = "white", bold = true}, commands[1])
+		end
+		
+		chatFunctions.privateMessage({text = commands[2] .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z, color = "white", bold = true}, commands[1])
 		chatFunctions.privateMessage({text = "Distance: " .. math.sqrt((table.x - user.x)^2 + (table.y - user.y)^2 + (table.z - user.z)^2) .. " Blocks", color = "white", bold = true}, commands[1])
 
-    end
+	end
 
 }
 
@@ -57,7 +57,7 @@ while true do
 		local mainCommand = commands[1]
 		table.remove(commands,1)
 
-        table.insert(commands, 1, user)
+		table.insert(commands, 1, user)
 		
 		if functions[mainCommand] ~= nil then
 			functions[mainCommand](commands)
