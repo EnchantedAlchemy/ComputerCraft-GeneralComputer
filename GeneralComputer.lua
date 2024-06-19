8ae87811e170e480d8f207114a54f40967e7b743
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-19 13:02:55 -0500

    Update GeneralComputer.lua

--- GeneralComputer.lua
+++ GeneralComputer.lua
@@ -37,11 +37,7 @@ functions = {
 			return
 		end
 		
-		chatFunctions.privateMessage({text = commands[2] .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z, color = "white", bold = true}, commands[1])
-		os.sleep(1)
-		print("hi")
-		chatFunctions.privateMessage({text = commands[2] .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z, color = "white", bold = true}, commands[1])
-		chatFunctions.privateMessage({text = "Distance: " .. distance .. " Blocks", color = "white", bold = true}, commands[1])
+		chatFunctions.privateMessage({text = commands[2] .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z .. " | Distance: " .. distance .. " Blocks", color = "white", bold = true}, commands[1])
 
 	end
 
