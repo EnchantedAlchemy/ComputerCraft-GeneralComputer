e991dd90dd55fefd7b577ea7014d1545a84e7926
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-19 14:57:42 -0500

    Update GeneralComputer.lua

--- GeneralComputer.lua
+++ GeneralComputer.lua
@@ -1,20 +1,11 @@
 --Requires:
 --Chatbox
---Chest above Computer
---Inventory Manager
+--Player Detector
 
-local manager = peripheral.find("inventoryManager")
-local owner = manager.getOwner()
+local textFunctions = require("utilities/textFunctions")
+local det = peripheral.find("playerDetector")
 local chatBox = peripheral.find("chatBox")
-local chatBoxName = "Inventory Manager"
-local takePeripheral = peripheral.find("minecraft:chest")
-local takeName = peripheral.getName(takePeripheral)
-local takeChest = "up"
-local trash = "down"
-
-print("\nType \"$help\" or \"$help (command name)\" for information.")
-
-redstone.setOutput("bottom",false)
+local chatBoxName = "General"
 
 chatFunctions = {
 
@@ -29,179 +20,99 @@ chatFunctions = {
 
 }
 
-chatFunctions.privateMessage({text = "Finished Loading.", color = "aqua", bold = true}, owner)
-
 functions = {
 
-	take = function(commands)
-	
-		--No item given
-		if commands[1] == nil or commands[1] == "" then
-			chatFunctions.privateMessage({text = "Enter an item.", color = "red", bold = true}, owner)
+	ping = function(commands)
+
+		local desiredName = commands[2]
+		local registerCaps = commands[3]
+
+		if registerCaps == nil or string.lower(registerCaps) ~= "true" then registerCaps = "false" end
+
+		--No player given
+		if desiredName == nil or desiredName == "" then
+			chatFunctions.privateMessage({text = "Enter an player username.", color = "red", bold = true}, commands[1])
 			return
 		end
-	
-		--Main command
-		local desiredItem = string.lower(commands[1])
-		local desiredQuantity = tonumber(commands[2])
-		
-		if desiredQuantity == nil or desiredQuantity < 1 then 
-			desiredQuantity = 1 
+
+		local name = ""
+		local players = det.getOnlinePlayers()
+
+		if registerCaps == "false" then
+			desiredName = string.lower(desiredName)
 		end
-		
-		if manager.getFreeSlot() ~= -1 then
-		
-			local item = ""
-			for i,v in pairs(takePeripheral.list()) do
-				if string.find(v.name, desiredItem) then
-					if item == "" or string.len(v.name) < string.len(item) then
-						item = v.name
-					end
-				end	
+
+		for i,v in pairs(players) do
+			local compareName = v
+			if registerCaps == "false" then 
+				compareName = string.lower(v) 
 			end
-		
-			local value, value2 = manager.addItemToPlayer(takeChest, desiredQuantity, nil, item)
-			--Successful
-			if value > 0 and value2 == nil then
-				chatFunctions.privateMessage({text = "Took "..value.." "..item..".", color = "green", bold = true}, owner)
-			--Incorrect Command
-			elseif value2 == "ITEM_NOT_FOUND" then
-				chatFunctions.privateMessage({text = "Improper item name (mod:item_name)", color = "red", bold = true}, owner)
-			--Chest does not have item
-			elseif value2 == nil then
-				chatFunctions.privateMessage({text = "Chest does not contain item.", color = "red", bold = true}, owner)
-			--Other error
-			else
-				chatFunctions.privateMessage({text = "Error (Show Gamer):".. value2, color = "red", bold = true}, owner)
-			end
-		--No space in inventory
-		else
-			chatFunctions.privateMessage({text = "No empty space in inventory.", color = "red", bold = true}, owner)
+			if string.find(compareName, desiredName) then
+				if name == "" or string.len(v) < string.len(name) then
+					name = v
+				end
+			end	
 		end
-		
-	end,
-	
-	store = function(commands)
-	
-		local desiredItem = manager.getItemInHand()
-		local desiredQuantity = tonumber(commands[1])
-		
-		if desiredQuantity == nil or desiredQuantity < 1 then 
-			desiredQuantity = desiredItem.count
+
+		local table = det.getPlayerPos(name)
+		local user = det.getPlayerPos(commands[1])
+
+		if table.x == nil then
+			chatFunctions.privateMessage({text = "Invalid username or player is in another dimension.", color = "red", bold = true}, commands[1])
+			return
 		end
+
+		local distance = math.sqrt((table.x - user.x)^2 + (table.y - user.y)^2 + (table.z - user.z)^2)
 		
-		if desiredItem.name ~= nil then
-			local value, value2 = manager.removeItemFromPlayer(takeChest, desiredQuantity, nil, desiredItem.name)
-			--Successful
-			if value > 0 and value2 == nil then
-				chatFunctions.privateMessage({text = "Stored "..value.." "..desiredItem.name..".", color = "green", bold = true}, owner)
-			--No Chest
-			elseif value2 == "INVENTORY_TO_INVALID" then
-				chatFunctions.privateMessage({text = "No chest to store items.", color = "red", bold = true}, owner)
-			--Chest has no storage
-			elseif value == 0 then
-				chatFunctions.privateMessage({text = "Chest is full.", color = "red", bold = true}, owner)
-			--Other Error
-			else
-				chatFunctions.privateMessage({text = "Error (Show Gamer):".. value2, color = "red", bold = true}, owner)
-			end
-		--No item in hand
-		else
-			local chatMessage = {
-				{text = "(Private) ", color = "gray", italic = true},
-				{text = "No item in hand.", color = "red", bold = true}
-			}
-			chatMessage = textutils.serializeJSON(chatMessage)
-			
-			chatBox.sendFormattedMessageToPlayer(chatMessage, owner, chatBoxName)	
-		end
-		
+		chatFunctions.privateMessage({text = "\n" .. name .. ": " .. table.x .. ", " .. table.y .. ", " .. table.z .. "\nDistance: " .. textFunctions.round(distance) .. " Blocks", color = "white", bold = true, italic = false}, commands[1])
+
 	end,
 
-	del = function(commands)
-	
-		local desiredItem = manager.getItemInHand()
-		local desiredQuantity = tonumber(commands[1])
-		
-		if desiredQuantity == nil or desiredQuantity < 1 then 
-			desiredQuantity = desiredItem.count
-		end
-		
-		if desiredItem.name ~= nil then
-			local value, value2 = manager.removeItemFromPlayer(trash, desiredQuantity, nil, desiredItem.name)
-			--Successful
-			if value > 0 and value2 == nil then
-				chatFunctions.privateMessage({text = "Deleted "..value.." "..desiredItem.name..".", color = "green", bold = true}, owner)
-				redstone.setOutput("bottom", false)
-				os.sleep(0.05)
-				redstone.setOutput("bottom", true)
-				os.sleep(0.05)
-				redstone.setOutput("bottom", false)
-			--No Chest
-			elseif value2 == "INVENTORY_TO_INVALID" then
-				chatFunctions.privateMessage({text = "No trashcan.", color = "red", bold = true}, owner)
-			--Chest has no storage
-			elseif value == 0 then
-				chatFunctions.privateMessage({text = "Trashcan is full?", color = "red", bold = true}, owner)
-			--Other Error
-			else
-				chatFunctions.privateMessage({text = "Error (Show Gamer):".. value2, color = "red", bold = true}, owner)
+	radar = function(commands)
+
+		local players = det.getPlayersInRange(300)
+		local displayedPlayers = ""
+
+		for i,v in pairs(players) do
+			if commands[2] == "true" or (v ~= "EnchantedAlchemy" and v ~= "garbloni" and v ~= "LogHammm") then
+				displayedPlayers = displayedPlayers .. v .. "\n"
 			end
-		--No item in hand
+		end
+
+		if displayedPlayers ~= "" then
+			chatFunctions.privateMessage({text = "Players within 300 blocks of detector:\n" .. displayedPlayers, color = "white", bold = false, italic = false}, commands[1])
 		else
-			local chatMessage = {
-				{text = "(Private) ", color = "gray", italic = true},
-				{text = "No item in hand.", color = "red", bold = true}
-			}
-			chatMessage = textutils.serializeJSON(chatMessage)
-			
-			chatBox.sendFormattedMessageToPlayer(chatMessage, owner, chatBoxName)	
+			chatFunctions.privateMessage({text = "No players within 300 blocks of detector.", color = "white", bold = false, italic = false}, commands[1])
 		end
 		
-	end,
-	
-	list = function(commands)
-		message = "\n"
-				
-		local chatMessage = {
-			{text = "\n"}
-		}
-		
-		for i,v in pairs(takePeripheral.list()) do
-			local textColor = "aqua"
-			if i%2 == 0 then
-				textColor = "white"
-			end
-			local slot = i
-			if slot < 10 then slot = "0"..slot end
-			chatMessage[#chatMessage+1] = {text = "[Slot "..slot.."] "..v.count.." "..v.name.."\n", color = textColor}
-		end
-		chatMessage = textutils.serializeJSON(chatMessage)
-		chatBox.sendFormattedMessageToPlayer(chatMessage, owner, chatBoxName)
+
 	end,
 
 	help = function(commands)
-		local argument = commands[1]
+
 		local chatMessage = {}
-		if argument == nil then
-			argument = ""
-		end
-		
-		
-		if string.find(argument, "inv")
+
+		if commands[2] ~= "general" then
+
+			chatMessage = {
+				{text = "Type \"$help\" followed by one of the following terms to see more info:\n", color = "white"},
+				{text = "general\n", color = "white"}, {text = "See info on general commands.\n", color = "gray"},
+				{text = "inv\n", color = "white"}, {text = "See info on inventory manager commands, if you are connected to one.\n", color = "gray"},
+			}
+
+		else
+
 			chatMessage = {
 				{text = "command | ", color = "white"}, {text = "required args.", color = "aqua"}, {text = " | ", color = "white"}, {text = "optional args.\n", color = "yellow"},
-				{text = "$take | ", color = "white"}, {text = "mod:item_name", color = "aqua"}, {text = " | ", color = "white"}, {text = "quantity\n", color = "yellow"}, {text = "Takes one (or given amount) of the given item if it is in storage.\n", color = "gray"},
-				{text = "$store | ", color = "white"}, {text = "quantity\n", color = "yellow"}, {text = "Puts held stack (or given amount) of items in storage.\n", color = "gray"},
-				{text = "$del | ", color = "white"}, {text = "quantity\n", color = "yellow"}, {text = "DELETES held stack (or given amount) of items.\n", color = "gray"},
-				{text = "$list\n", color = "white"}, {text = "Lists all items in storage\n", color = "gray"},
-				{text = "$help | ", color = "white"}, {text = "command name", color = "yellow"}
+				{text = "$ping | ", color = "white"}, {text = "username", color = "aqua"}, {text = " | ", color = "white"}, {text = "registersCapslock\n", color = "yellow"}, {text = "Display the given player's username. Not caps sensitive unless registersCapslock is true.\n", color = "gray"},
+				{text = "$radar | ", color = "white"}, {text = "includeFaction\n", color = "yellow"}, {text = "Displays players within 300 blocks of the computer. Does not include Penumbra unless includeFaction is true.\n", color = "gray"},
 			}
+
 		end
-		
+
 		chatMessage = textutils.serializeJSON(chatMessage)
-		chatBox.sendFormattedMessageToPlayer(chatMessage, owner, chatBoxName)
-		
+		chatBox.sendFormattedMessageToPlayer(chatMessage, commands[1], chatBoxName)
+
 	end
 
 }
@@ -209,8 +120,8 @@ functions = {
 while true do
 
 	local event, user, message, uuid, isHidden = os.pullEvent("chat")
-	if isHidden and user == owner then
-		
+	if isHidden and user == "EnchantedAlchemy" or user == "garbloni" or user == "LogHammm" then
+
 		local commands = {}
 		
 		for s in string.gmatch(message, "[%w:_]+") do
@@ -219,6 +130,8 @@ while true do
 		
 		local mainCommand = commands[1]
 		table.remove(commands,1)
+
+		table.insert(commands, 1, user)
 		
 		if functions[mainCommand] ~= nil then
 			functions[mainCommand](commands)
