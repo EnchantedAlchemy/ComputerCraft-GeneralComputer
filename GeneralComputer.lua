01f3a09ba2d2c57b8e188e186a9a90c6921c3ca7
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-20 16:34:46 -0500

    Update GeneralComputer.lua

--- GeneralComputer.lua
+++ GeneralComputer.lua
@@ -115,7 +115,7 @@ functions = {
 						numNearby = numNearby + 1
 
 						if numNearby % 2 == 0 then
-							nameColor = "white"
+							nameColor = "yellow"
 						else
 							nameColor = "aqua"
 						end
